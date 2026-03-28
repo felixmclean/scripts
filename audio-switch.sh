0d7a1e87cc634ccdeb12aa3abfbd8bcd448bff49
felix <felix@99w9.com>
2026-03-28 18:57:11 -0400

    init commit

---
+++ audio-switch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+SPEAKERS="alsa_output.pci-0000_2f_00.4.analog-stereo"
+EARBUDS="alsa_output.usb-Synaptics_MEZE_AUDIO-00.analog-stereo"
+
+current=$(pactl get-default-sink)
+
+if [ "$current" = "$SPEAKERS" ]; then
+    pactl set-default-sink "$EARBUDS"
+    notify-send "Audio: Earbuds"
+else
+    pactl set-default-sink "$SPEAKERS"
+    notify-send "Audio: Speakers"
+fi
+
