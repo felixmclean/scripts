da2cc0ca3856915421b05e6a883f5a59a4740783
felix <felix@99w9.com>
2026-06-17 10:36:38 -0400

    add persistent clipboard to autostart script

--- autostart.sh
+++ autostart.sh
@@ -2,3 +2,4 @@
 dbus-update-activation-environment --systemd WAYLAND_DISPLAY XDG_CURRENT_DESKTOP
 dunst &
 keepassxc /home/felix/Documents/KeePass/Passwords.kdbx &
+wl-clip-persist --clipboard regular &
